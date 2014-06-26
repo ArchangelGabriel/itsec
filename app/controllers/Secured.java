e7b626b98c8c90e39b6355542645b51808a1c8d0
Gabriel <juangab31@gmail.com>
2014-06-26 21:27:47 +0800

    First baseline commit v0.1

--- app/controllers/Secured.java
+++ app/controllers/Secured.java
@@ -1,13 +1,10 @@
 package controllers;
 
-import play.*;
 import play.mvc.*;
 import play.mvc.Http.*;
 
-import models.*;
 
 public class Secured extends Security.Authenticator {
-
     @Override
     public String getUsername(Context ctx) {
         return ctx.session().get("email");
---
+++ test/controllers/LoginTest.java
@@ -0,0 +1,58 @@
+package controllers;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.*;
+
+import play.mvc.*;
+import play.libs.*;
+import play.test.*;
+import static play.test.Helpers.*;
+import com.avaje.ebean.Ebean;
+import com.google.common.collect.ImmutableMap;
+
+public class LoginTest extends WithApplication {
+    @Test
+    public void authenticateSuccess() {
+        Result result = callAction(
+                controllers.routes.ref.Application.authenticate(),
+                fakeRequest().withFormUrlEncodedBody(ImmutableMap.of(
+                        "email", "bob@example.com",
+                        "password", "secret"))
+        );
+        assertEquals(303, status(result));
+        assertEquals("bob@example.com", session(result).get("email"));
+    }
+
+    @Test
+    public void authenticationFailure() {
+        Result result = callAction(
+                controllers.routes.ref.Application.authenticate(),
+                fakeRequest().withFormUrlEncodedBody(ImmutableMap.of(
+                        "email", "bob@example.com",
+                        "password", "badpassword"
+                ))
+        );
+        assertEquals(400, status(result));
+        assertNull(session(result).get("email"));
+    }
+
+    @Test
+    public void authenticated() {
+        Result result = callAction(
+                controllers.routes.ref.Application.index(),
+                fakeRequest().withSession("email", "bob@example.com")
+        );
+        assertEquals(200, status(result));
+    }
+
+    @Test
+    public void notAuthenticated() {
+        Result result = callAction(
+                controllers.routes.ref.Application.index(),
+                fakeRequest()
+        );
+        assertEquals(303, status(result));
+        assertEquals("/login", header("Location", result));
+    }
+}
